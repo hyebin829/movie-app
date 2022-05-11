79f0beec46c70517184842dd88f8d885a00ad1d0
hyebin829 <smilerain779@gmail.com>
2022-05-11 12:30:01 +0900

    Add: recoilRoot

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.scss'
 import Routes from './routes'
 import reportWebVitals from './reportWebVitals'
+import { RecoilRoot } from 'recoil'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Routes />
+    <RecoilRoot>
+      <Routes />
+    </RecoilRoot>
   </React.StrictMode>
 )
 
