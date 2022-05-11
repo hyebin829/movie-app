90e1fb324825791acbf45143be4b6255f3ed77af
hyebin829 <smilerain779@gmail.com>
2022-05-11 12:34:03 +0900

    Feat: routing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Main />} />
-          <Route path='/favorites' element={<Favorites />} />
+          <Route path='favorites' element={<Favorites />} />
         </Routes>
       </BrowserRouter>
     </div>
