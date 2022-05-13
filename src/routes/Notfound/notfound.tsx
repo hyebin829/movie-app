4bdd5e40a6b2230f119f23ae558eed2264358f8d
hyebin829 <smilerain779@gmail.com>
2022-05-13 23:35:15 +0900

    Add 404 page

---
+++ src/routes/Notfound/notfound.tsx
@@ -0,0 +1,12 @@
+import styles from './NotFound.module.scss'
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFound}>
+      <h1>404</h1>
+      <p>Page Not Found!</p>
+    </div>
+  )
+}
+
+export default NotFound
