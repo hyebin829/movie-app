821d222b9e051efca86a86aca05372ae16b47c25
hyebin829 <smilerain779@gmail.com>
2022-05-13 23:43:14 +0900

    Feat : loading page, 404 page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,18 +1,29 @@
-import styles from './Routes.module.scss'
-import Main from './Main'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
-import Favorites from './Favorites'
+import { Suspense, lazy } from 'react'
+
+import LoadingPage from './components/loading/loadingPage'
+import GNB from './components/GNB'
+
+import styles from './Routes.module.scss'
+
+const Main = lazy(() => import('./Main/index'))
+const Favorites = lazy(() => import('./Favorites/index'))
+const NotFound = lazy(() => import('./Notfound/notfound'))
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='favorites' element={<Favorites />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <Suspense fallback={<LoadingPage />}>
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='favorites' element={<Favorites />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        <GNB />
+      </div>
+    </BrowserRouter>
   )
 }
 
