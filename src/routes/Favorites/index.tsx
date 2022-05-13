da8501f0a21d118837c1905addb1c46b2ca302c6
hyebin829 <smilerain779@gmail.com>
2022-05-13 23:32:34 +0900

    Add favorites page

--- src/routes/Favorites/index.tsx
+++ src/routes/Favorites/index.tsx
@@ -1,7 +1,36 @@
+import { Fragment } from 'react'
+import { useRecoilValue } from 'recoil'
+
+import { ModalVisibleState } from 'state'
+import { IsearchResult } from 'types/movie'
+
+import FavoriteButton from 'routes/components/favoriteButton/favoriteButton'
+import Modal from 'routes/components/modal/modal'
+import Movie from 'routes/Main/movie'
 import styles from './Favorites.module.scss'
 
 const Favorites = () => {
-  return <div>favorite</div>
+  const favorite = localStorage.getItem('favorite')
+  const favoriteList = favorite ? JSON.parse(favorite) : []
+  const modalVisible = useRecoilValue(ModalVisibleState)
+
+  return (
+    <>
+      {modalVisible && <Modal />}
+      <div className={styles.favoritesWrapper}>
+        <h1>&#x2B50; Favorite List &#x2B50;</h1>
+        <div className={styles.favoriteList}>
+          {favoriteList.map((item: IsearchResult, index: number) => (
+            <Fragment key={`${item.imdbID}-movie-${index + 1}`}>
+              <Movie item={item}>
+                <FavoriteButton item={item} />
+              </Movie>
+            </Fragment>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Favorites
