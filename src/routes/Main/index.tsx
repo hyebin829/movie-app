5e0db21e24e6eee168d5a9f4e1e3fdae6d829415
hyebin829 <smilerain779@gmail.com>
2022-05-13 23:40:40 +0900

    Add: infinite scroll

--- src/routes/Main/index.tsx
+++ src/routes/Main/index.tsx
@@ -1,62 +1,96 @@
+import { ChangeEvent, useEffect, FormEvent, useState, Fragment } from 'react'
 import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { ChangeEvent, useCallback, useEffect, FormEvent } from 'react'
+import { useInView } from 'react-intersection-observer'
 
-import { MovieDataState, SearchWordState } from 'state'
-import { getMovieApi } from 'services/movie'
+import { ModalVisibleState, MovieDataState, PageNumState, SearchWordState, TotalResultNum } from 'state'
+import { useGetMovie } from 'services/movie'
+import { IsearchResult } from 'types/movie'
 
+import Modal from '../components/modal/modal'
+import Movie from './movie'
+import FavoriteButton from 'routes/components/favoriteButton/favoriteButton'
+import Loading from 'routes/components/loading'
 import styles from './Main.module.scss'
+import { BsSearch } from 'react-icons/bs'
 
 const Main = () => {
-  const movieData = useRecoilValue(MovieDataState)
+  const movieData = useRecoilValue<IsearchResult[]>(MovieDataState)
   const setMovieData = useSetRecoilState(MovieDataState)
-
-  const searchWord = useRecoilValue(SearchWordState)
   const setSearchWord = useSetRecoilState(SearchWordState)
 
-  useEffect(() => {}, [movieData, setMovieData])
+  const [inputValue, setInputValue] = useState('')
 
-  const getMovie = useCallback(() => {
-    getMovieApi({
-      movieName: searchWord,
-      page: 1,
-    }).then((res) => {
-      if (res.data.Response === 'False') setMovieData([])
-      setMovieData(res.data.Search)
-    })
-  }, [searchWord, setMovieData])
+  const setPageNum = useSetRecoilState(PageNumState)
+  const totalResultNum = useRecoilValue(TotalResultNum)
 
-  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    setSearchWord(e.target.value)
-  }
+  const modalVisible = useRecoilValue(ModalVisibleState)
+
+  const [target, inView] = useInView({
+    threshold: 1,
+  })
+
+  const { hasMore, loading } = useGetMovie()
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    getMovie()
+    setSearchWord(inputValue)
+    setMovieData([])
+    setPageNum(1)
+  }
+  useEffect(() => {
+    if (inView && hasMore && !loading && movieData.length < totalResultNum) {
+      setPageNum((prev) => prev + 1)
+    }
+  }, [hasMore, loading, inView, setPageNum, totalResultNum, movieData.length])
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    setInputValue(e.target.value)
+  }
+
+  const handleClickSearch = () => {
+    setSearchWord(inputValue)
+    setMovieData([])
+    setPageNum(1)
   }
 
   return (
-    <section className={styles.weather}>
-      <div className={styles.forecast}>
-        <h2>Next forecast</h2>
-        <form onSubmit={handleSubmit}>
-          <input type='text' value={searchWord} onChange={onChangeInput} />
-        </form>
-        <button onClick={getMovie} type='button'>
-          버튼
-        </button>
-        {movieData &&
-          movieData.map((item) => (
-            <ul key={`${item.imdbID}-movie`}>
-              <li>{item.Title}</li>
-              <li>{item.Year}</li>
-              <li>{item.Type}</li>
-              <img src={item.Poster} alt='moviePoster' />
-            </ul>
-          ))}
-        {!movieData && <div>noting</div>}
+    <>
+      {modalVisible && <Modal />}
+      <div className={styles.mainWrapper}>
+        <div className={styles.mainFixed}>
+          <h1>Search Movie ! &#x1F3AC;</h1>
+          <form onSubmit={handleSubmit} className={styles.inputWrapper}>
+            <input
+              className={styles.searchInput}
+              type='text'
+              placeholder='Search'
+              value={inputValue}
+              onChange={onChangeInput}
+            />
+            <button onClick={handleClickSearch} type='button' className={styles.searchButton}>
+              <BsSearch size={20} color='white' />
+            </button>
+          </form>
+        </div>
+        <section>
+          {movieData &&
+            movieData.map((item, index) => (
+              <Fragment key={`${item.imdbID}-movie-${index + 1}`}>
+                <Movie item={item}>
+                  <FavoriteButton item={item} />
+                </Movie>
+              </Fragment>
+            ))}
+          {!movieData.length && !loading ? <div className={styles.notFoundData}>검색 결과가 없습니다.</div> : <div />}
+          {loading && (
+            // <div className={styles.loading}>Loading... </div>
+            <Loading />
+          )}
+          <div ref={target} className={styles.refdiv} />
+        </section>
       </div>
-    </section>
+    </>
   )
 }
 
